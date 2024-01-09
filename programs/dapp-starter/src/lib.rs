142d7ca8a37b5e8110f6cab40ccb0ab0d5a20f7f
Trần Thành Trung <trungttnd@gmail.com>
2024-01-09 16:28:36 +0700

    create round

--- programs/dapp-starter/src/lib.rs
+++ programs/dapp-starter/src/lib.rs
@@ -14,6 +14,22 @@ pub mod dapp_starter {
         config.count += 1;
         Ok(())
     }
+    //create round
+    pub fn create_round(
+        _ctx: Context<CreateRound>,
+        round_id: u43
+    ) -> ProgramResult {
+        let config = &mut _ctx.accounts.config;
+        let round = &mut _ctx.accounts.round;
+        let clock: Clock = Clock::get()?;
+
+        config.count += 1;
+        round.id = config.count;
+        round.created_at = created_at;
+        round.updated_at = clock.unix_timestamp;
+        round.waiting_time = waiting_time;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -45,4 +61,52 @@ pub struct Increment<'info>{
 #[account]
 pub struct Counter {
     pub count: u64,
+}
+
+#[derive(Accounts)]
+#[instruction(round_id: u32)]
+pub struct CreateRound<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(
+        init,
+        payer = payer,
+        space = 8 + 4 + 1 + 1 + 4 + 8 + 8 + 1 + 8 + 8 + 8 + 8 + 1 + 1000 * (4 + 4 + 1 + 32 + 1 + 8 + 8 + 8 + 8),
+        seeds = [ROUND_SEED.as_bytes(), &round_id.to_le_bytes()],
+        bump,
+    )]
+    pub round: Account<'info, Round>,
+    #[account(mut)]
+    pub config: Account<'info, Counter>, 
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Round {
+    pub id: u32, //4
+    pub drawn: bool,//1
+    pub closed: bool,//1
+    pub last_ticket_id: u32,//4
+    pub winning_number: u64,//8
+    pub bet_amount: u64,//8
+    pub waiting_time: u8,//1
+    pub created_at: i64,//8
+    pub updated_at: i64,//8
+    pub small_total_amount: u64,
+    pub big_total_amount: u64,
+    pub bet_result: u8,
+    pub tickets: Vec<Ticket>
+}
+
+#[account]
+pub struct Ticket {
+    pub id: u32,
+    pub round_id: u32,
+    pub claimed: bool,
+    pub authority: Pubkey,
+    pub bet_value: u8,
+    pub bet_amount: u64,
+    pub unaccepted_amount: u64,
+    pub created_at: i64,
+    pub win_amount: u64,
 }
